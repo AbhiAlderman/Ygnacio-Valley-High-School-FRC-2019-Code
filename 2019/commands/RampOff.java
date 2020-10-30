1acf5fd8eeb80a224f3b751fdecdfcf8cfccf2fb
Abhi Alderman <60452804+AbhiAlderman@users.noreply.github.com>
2020-10-30 14:44:33 -0700

    Update RampOff.java

--- 2019/commands/RampOff.java
+++ 2019/commands/RampOff.java
@@ -10,6 +10,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
+//Command to lower ramp
 public class RampOff extends Command {
   public RampOff() {
     requires(Robot.rampsub);

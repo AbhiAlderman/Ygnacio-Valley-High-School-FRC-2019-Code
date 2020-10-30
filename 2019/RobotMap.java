15ad7f14baad504618721c7afa21c312fa37e552
Abhi Alderman <60452804+AbhiAlderman@users.noreply.github.com>
2020-10-30 13:15:50 -0700

    Update RobotMap.java

--- 2019/RobotMap.java
+++ 2019/RobotMap.java
@@ -24,22 +24,45 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * floating around.
  */
 public class RobotMap {
+  //Motors for the Drivetrain
   public static WPI_TalonSRX frontRight = new WPI_TalonSRX(4);
   public static WPI_TalonSRX backRight = new WPI_TalonSRX(5);
   public static WPI_TalonSRX frontLeft = new WPI_TalonSRX(6);
   public static WPI_TalonSRX backLeft = new WPI_TalonSRX(7);
+  
+  //Divide the 4 motors into 2 groups
   public static SpeedControllerGroup leftWheels =  new SpeedControllerGroup(frontLeft, backLeft);
   public static SpeedControllerGroup rightWheels = new SpeedControllerGroup(frontRight, backRight);
+  
+  //Create a drivetrain from the 2 groups
   public static DifferentialDrive drivetrain = new DifferentialDrive(leftWheels, rightWheels);
+  
+  //Motors for the intake
   public static WPI_TalonSRX rightIntake = new WPI_TalonSRX(1);
   public static WPI_TalonSRX leftIntake = new WPI_TalonSRX(2);
+  
+  //Motor for the arm
   public static WPI_TalonSRX armMotor = new WPI_TalonSRX(8);
+  
+  //Solenoid to shift gears on the drivetrain
   public static Solenoid gearbox = new Solenoid(0);
+  
+  //Solenoid to raise the ramp
   public static Solenoid ramp = new Solenoid(1);
+  
+  //Solenoid to extend the piston
   public static Solenoid intakepiston = new Solenoid(2);
+  
+  
   public static Solenoid hatchlevel = new Solenoid(3);
+  
+  //Solenoid to raise and lower arm
   public static DoubleSolenoid arm = new DoubleSolenoid(4, 5);
+  
+  //Compressor to give air to solenoids
   public static Compressor compressor = new Compressor();
+  
+  //Cameras
   public static UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
   public static UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
 

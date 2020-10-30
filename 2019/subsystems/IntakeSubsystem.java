a02b7b04fa40dea830a14e4757eaf9c1a122aceb
Abhi Alderman <60452804+AbhiAlderman@users.noreply.github.com>
2020-10-30 13:38:14 -0700

    Update IntakeSubsystem.java

--- 2019/subsystems/IntakeSubsystem.java
+++ 2019/subsystems/IntakeSubsystem.java
@@ -17,24 +17,31 @@ import frc.robot.commands.IntakeMotorCommand;
 /**
  * Add your docs here.
  */
+
+//Subsystem for the Intake Motors
 public class IntakeSubsystem extends Subsystem {
+  //Create the intake motors
   WPI_TalonSRX rightIntake, leftIntake;
 
+  //Assign the intake motors
   public IntakeSubsystem(){
     rightIntake = RobotMap.rightIntake;
     leftIntake = RobotMap.leftIntake;
   }
 
+  //Method for controlling the left intake motor
   public void leftIntakeSet(double x){
     leftIntake.set(ControlMode.PercentOutput, x); //reverse the left side
   }
 
+  //Method for controlling the right intake motor
   public void rightIntakeSet(double x){
     rightIntake.set(ControlMode.PercentOutput, x);
   }
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  //Set the default command
   @Override
   public void initDefaultCommand() {
     setDefaultCommand(new IntakeMotorCommand());
